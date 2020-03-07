92f187d61e382e437209f05ee537e6f9b8e24ffa
AdavizeTheFirst <42492202+AdavizeTheFirst@users.noreply.github.com>
2020-03-07 11:39:11 +0100

    uploaded work files

---
+++ assignment/week-3/adavize/index.js
@@ -0,0 +1,19 @@
+let dayArr = ["Saturday" ,'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday'];
+let dd = document.getElementById ("dd");
+let hh = document.getElementById("hh");
+let mm = document.getElementById("mm");
+let ss = document.getElementById("ss");
+
+let display = function (day, hr, mn, sec) {
+ dd.innerText = dayArr[day];
+ hh.innerText = hr;
+ mm.innerText = mn;
+ ss.innerText = sec;
+}
+
+let updateDisplay = function(){
+  let dateObj = new Date();
+  display(dateObj.getDay(), dateObj.getHours(), dateObj.getMinutes(), dateObj.getSeconds());
+}
+
+setInterval(updateDisplay, 1000);
